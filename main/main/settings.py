1bc6f290f651b5f1e6368a9c0c0d73e75d24bedc
Ivan <mr.grifon345@mail.ru>
2025-06-25 20:34:50 +0300

    qqqqqw

--- main/main/settings.py
+++ main/main/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/itdesigners.online/test_deploy/main/staticfiles/'
+
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/itdesigners.online/test_deploy/main/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
